6016376a3bef035a4a93bba9a3b2750c7033e6dd
Your Name <davidfrank96>
2020-01-24 17:44:34 -0800

    SettingUp the routes the folder Structure app.js/ Entry File

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+
+
+const apiRoutes = express.Router();
+
+apiRoutes.get('/', (req, res) =>
+  res.json({
+    status: 200,
+    message: 'Welcome to Notification Service'
+  })
+);
+
+apiRoutes.get('/v1', (req, res) =>
+  res.json({
+    status: 200,
+    message: 'Welcome to version 1 ofNotification Service'
+  })
+);
+
+
+
+
+export default apiRoutes;
